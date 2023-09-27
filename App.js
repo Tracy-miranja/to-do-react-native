479b429f982c8305753821b25cfe7e5b7b68a9fb
Tracy-miranja <barazatracy44@gmail.com>
2023-09-27 14:51:42 +0300

    rendered list

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [todos, setTodos]= useState([
+    {text:'buy brakfast', key:'1'},
+    {text:'go shopping', key:'2'},
+    {text:'buy sapper', key:'3'},
+    {text:'Arrange the table', key:'4'}
+  ])
   return (
     <View style={styles.container}>
-     
+     {/* header*/}
+     <View style={styles.content}>
+      {/*to do form*/}
+      <View style={styles.lists}>
+        <FlatList
+          data={todos}
+          renderItem={({item})=>(
+            <Text>{item.text}</Text>
+          )}
+        />
+      </View>
+     </View>
     </View>
   );
 }
@@ -16,4 +34,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  content: {
+    padding: 40.
+  },
+  list:{
+    margin:20
+  }
+
 });
